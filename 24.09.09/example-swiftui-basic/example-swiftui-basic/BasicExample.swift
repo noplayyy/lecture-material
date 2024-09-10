5ae8f59828f9548e5462fa5eac909ad8538b3726
노연주 <noplay1016@gmail.com>
2024-09-10 11:40:09 +0900

    add: 24.09.09 2차시 예제 프로젝트

---
+++ 24.09.09/example-swiftui-basic/example-swiftui-basic/BasicExample.swift
@@ -0,0 +1,88 @@
+//
+//  BasicExample.swift
+//  2-example-swiftui
+//
+//  Created by Yeonju on 9/8/24.
+//
+
+import SwiftUI
+
+// MARK: - Navigation Destination
+enum Basic {
+    case text
+    case button
+    case image
+}
+
+struct BasicExample: View {
+    var type: Basic
+    
+    init(_ type: Basic) {
+        self.type = type
+    }
+    
+    var body: some View {
+        Group {
+            switch type {
+            case .text:
+                TextExampleView()
+            case .button:
+                ButtonExampleView()
+            case .image:
+                ImageExampleView()
+            }
+        }
+        .ignoresSafeArea()
+    }
+}
+
+// MARK: - Text
+struct TextExampleView: View {
+    var body: some View {
+        Text("Text\nExample\n!-!")
+            .font(.largeTitle)
+            .padding(20)
+            .frame(alignment: .center)
+            .multilineTextAlignment(.center)
+    }
+}
+
+// MARK: - Button
+struct ButtonExampleView: View {
+    var body: some View {
+        Button {
+            print("Button click !!!")
+        } label: {
+            Text("Button Example")
+                .fontWeight(.heavy)
+                .font(.title3)
+                .foregroundStyle(Color.white)
+                .frame(width: 300, height: 300)
+                .background(Color.brown)
+                .cornerRadius(50)
+        }
+    }
+}
+
+// MARK: - Image
+struct ImageExampleView: View {
+    var body: some View {
+        Image(.lovelyDog)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .clipShape(Circle())
+    }
+}
+
+// MARK: - Previews
+#Preview("Text") {
+    TextExampleView()
+}
+
+#Preview("Button") {
+    ButtonExampleView()
+}
+
+#Preview("Image") {
+    ImageExampleView()
+}
---
+++ 24.09.09/example-swiftui-basic/example-swiftui-basic/ContentView.swift
@@ -0,0 +1,49 @@
+//
+//  ContentView.swift
+//  2-example-swiftui
+//
+//  Created by Yeonju on 9/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationStack {
+            List {
+                Section(
+                    header: {
+                        Text("Basic")
+                            .font(.title3)
+                            .fontWeight(.bold)
+                    }()
+                ) {
+                    NavigationLink("Text", value: Basic.text)
+                    NavigationLink("Button", value: Basic.button)
+                    NavigationLink("Image", value: Basic.image)
+                }
+                Section(
+                    header: {
+                        Text("Stack")
+                            .font(.title3)
+                            .fontWeight(.bold)
+                    }()
+                ) {
+                    NavigationLink("VStack", value: Stack.vstack)
+                    NavigationLink("HStack", value: Stack.hstack)
+                    NavigationLink("ZStack", value: Stack.zstack)
+                }
+            }
+            .navigationDestination(for: Basic.self) { type in
+                BasicExample(type)
+            }
+            .navigationDestination(for: Stack.self) { type in
+                StackExample(type)
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
