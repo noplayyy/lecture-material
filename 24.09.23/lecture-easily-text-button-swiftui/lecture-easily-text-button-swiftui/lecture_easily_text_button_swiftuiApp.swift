e324391d5561a1c3d5a25351c12f5021d6adf67d
노연주 <noplay1016@gmail.com>
2024-09-23 22:15:26 +0900

    add: 24.09.23 3차시 강의 실습 프로젝트 - SwiftUI

---
+++ 24.09.23/lecture-easily-text-button-swiftui/lecture-easily-text-button-swiftui/lecture_easily_text_button_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  lecture_easily_text_button_swiftuiApp.swift
+//  lecture-easily-text-button-swiftui
+//
+//  Created by Yeonju on 9/23/24.
+//
+
+import SwiftUI
+
+@main
+struct lecture_easily_text_button_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
