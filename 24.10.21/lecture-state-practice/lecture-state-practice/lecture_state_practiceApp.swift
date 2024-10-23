096282bc543c7ec2eb7d6ef782b68a9397e2cacc
노연주 <noplay1016@gmail.com>
2024-10-23 17:14:08 +0900

    add: 24.10.21 5차시 강의 실습 프로젝트

---
+++ 24.10.21/lecture-state-practice/lecture-state-practice/lecture_state_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  lecture_state_practiceApp.swift
+//  lecture-state-practice
+//
+//  Created by Yeonju on 10/21/24.
+//
+
+import SwiftUI
+
+@main
+struct lecture_state_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
