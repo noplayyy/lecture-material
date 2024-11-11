656976ffb5898ce6e055e64f09538b6fd5a1621a
노연주 <noplay1016@gmail.com>
2024-11-11 10:35:39 +0900

    add: 24.11.04 7차시 강의 과제 프로젝트
    TODO: - 8차시에 View 구성 및 increaseLike 로직 구현

---
+++ 24.11.04/lecture-object-practice/lecture-object-practice/lecture_object_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  lecture_object_practiceApp.swift
+//  lecture-object-practice
+//
+//  Created by Yeonju on 11/4/24.
+//
+
+import SwiftUI
+
+@main
+struct lecture_object_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
